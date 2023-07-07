4e575bf72e8bf4be685d366082c184b72e5be92b
Shubham Desai <shubham.desai@torinit.ca>
2023-07-07 19:00:31 +0530

    route changed

--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,7 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
-    children: [{ path: "/home", element: <Home /> }],
+    children: [{ path: "home", element: <Home /> }],
   },
 ]);
 
