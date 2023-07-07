4ab24b29b3a4fd076c4d1ddcc0bbfde88501d42a
Shubham Desai <shubham.desai@torinit.ca>
2023-07-07 18:45:02 +0530

    text changed

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React with Typescript
         </a>
       </header>
     </div>
