79fcc39e02d2bc1cdda6f740432c6970e1f101a5
Shubham Desai <shubham.desai@torinit.ca>
2023-07-10 13:26:01 +0530

    auth added

--- src/components/shared-containers/LoginCardContainer/index.tsx
+++ src/components/shared-containers/LoginCardContainer/index.tsx
@@ -1,6 +1,8 @@
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { saveUserToken } from "../../../reducers/UserReducer";
+import { GoogleLogin } from "@react-oauth/google";
+// const clientId = process.env.REACT_APP_GOOGLE_CLIENT_ID as string;
 
 const LoginCardContainer = () => {
   const dispatch = useDispatch();
@@ -10,10 +12,18 @@ const LoginCardContainer = () => {
     dispatch(saveUserToken({ token: "User" }));
     navigate("/");
   };
+  const responseMessage = (response: any) => {
+    console.log(response);
+  };
+  const errorMessage = () => {
+    console.log("error");
+  };
 
   return (
     <div className="h-1/4 w-4/5 justify-around flex flex-col bg-white rounded-2xl ">
       <h4 className="text-center ">Login</h4>
+      <GoogleLogin onSuccess={responseMessage} onError={errorMessage} />
+
       <button
         onClick={loginHandler}
         className=" m-2 text-2xl text-amber-600 p-2 rounded-xl hover:text-white hover:bg-rose-500 focus:ring focus:ring-violet-300 "
--- src/index.tsx
+++ src/index.tsx
@@ -6,16 +6,26 @@ import reportWebVitals from "./reportWebVitals";
 import router from "./routes/AppRoutes";
 import { Provider } from "react-redux";
 import store from "./ducks/store";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+
+// const clientId = process.env.REACT_APP_GOOGLE_CLIENT_ID as string;
+// console.log(clientId);
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <RouterProvider router={router} />
-    </Provider>
-  </React.StrictMode>
+  <GoogleOAuthProvider
+    clientId={
+      "1023699383316-luoam7mm3j520rc1ek485jke2du0jpma.apps.googleusercontent.com"
+    }
+  >
+    <React.StrictMode>
+      <Provider store={store}>
+        <RouterProvider router={router} />
+      </Provider>
+    </React.StrictMode>
+  </GoogleOAuthProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
