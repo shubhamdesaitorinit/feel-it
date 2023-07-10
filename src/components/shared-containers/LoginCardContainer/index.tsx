400cc7daf2262dde0b545937d261f2dbed2a5844
Shubham Desai <shubham.desai@torinit.ca>
2023-07-10 14:09:47 +0530

    authenction test

--- src/components/shared-containers/LoginCardContainer/index.tsx
+++ src/components/shared-containers/LoginCardContainer/index.tsx
@@ -1,23 +1,65 @@
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { saveUserToken } from "../../../reducers/UserReducer";
-import { GoogleLogin } from "@react-oauth/google";
+import { GoogleLogin, googleLogout, useGoogleLogin } from "@react-oauth/google";
+import { useEffect, useState } from "react";
+import axios from "axios";
 // const clientId = process.env.REACT_APP_GOOGLE_CLIENT_ID as string;
+interface User {
+  credential: string;
+}
 
+interface Profile {
+  email: string;
+}
 const LoginCardContainer = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const [user, setUser] = useState<User | any>();
 
-  const loginHandler = () => {
-    dispatch(saveUserToken({ token: "User" }));
-    navigate("/");
-  };
+  const [profile, setProfile] = useState<Profile | null>();
+
+  // const loginHandler = () => {
+  //   dispatch(saveUserToken({ token: profile?.email }));
+  //   navigate("/");
+  // };
   const responseMessage = (response: any) => {
+    setUser(response);
+
     console.log(response);
   };
   const errorMessage = () => {
     console.log("error");
   };
+
+  const login = useGoogleLogin({
+    onSuccess: (codeResponse) => setUser(codeResponse),
+    onError: (error) => console.log("Login Failed:", error),
+  });
+
+  const logOut = () => {
+    googleLogout();
+    setProfile(null);
+  };
+
+  useEffect(() => {
+    if (user) {
+      axios
+        .get(
+          `https://www.googleapis.com/oauth2/v1/userinfo?credential=${user.credential}`,
+          {
+            headers: {
+              Authorization: `Bearer ${user?.credential}`,
+              Accept: "application/json",
+            },
+          }
+        )
+        .then((res) => {
+          setProfile(res.data);
+        })
+        .catch((err) => console.log(err));
+    }
+  }, [user]);
 
   return (
     <div className="h-1/4 w-4/5 justify-around flex flex-col bg-white rounded-2xl ">
@@ -25,7 +67,7 @@ const LoginCardContainer = () => {
       <GoogleLogin onSuccess={responseMessage} onError={errorMessage} />
 
       <button
-        onClick={loginHandler}
+        onClick={() => {}}
         className=" m-2 text-2xl text-amber-600 p-2 rounded-xl hover:text-white hover:bg-rose-500 focus:ring focus:ring-violet-300 "
       >
         Google
